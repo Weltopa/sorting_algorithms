c79270bfe4dad52d78a96fa9661b89eef6b24ff9
Weltopa <patrick.welton@yahoo.com>
2021-09-19 16:42:28 -0700

    Bubble Sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,9 @@
+#include "sort.h"
+/**
+ * bubble_sort - Sorts an array of ints in ascending order. 
+ * @array: Array to sort.
+ * @size: Size of array.
+ */
+void bubble_sort(int *array, size_t size)
+{
+
