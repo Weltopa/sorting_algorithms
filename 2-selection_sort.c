e72505265a11668c361984dbbe8d0b5e5327242c
Weltopa <patrick.welton@yahoo.com>
2021-09-19 18:06:11 -0700

    fixing loop

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,7 +12,7 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 	{
 		n = i;
-		for (j = i = 1; j < size; j++)
+		for (j = i + 1; j < size; j++)
 			if (array[j] < array[n])
 				n = j;
 		if (n != i)
